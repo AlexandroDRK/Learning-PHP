b7ee495880f8a556c1149a27116f7113c5d4149f
AlexandroDRK <alexsandro060302@gmail.com>
2022-08-24 16:56:52 -0300

    UP do projeto + aula 1

---
+++ testserver - aula 1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8"/>
+    <title></title>
+</head>
+<body>
+    <?php
+        $nome = "Alexandro";
+        $idade = 20;
+        echo "$nome tem $idade anos!";
+    ?>
+</body>
+</html>
